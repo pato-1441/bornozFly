cc57384aaebddbce524ca55aa8e8b3378eab8e9a
Patricio Albornoz <wagnerpatricio@outlook.com.ar>
2023-01-20 13:19:49 -0300

    hicimos rollback porque estaba dando problemas, mail mandando funcionando

--- models/user.js
+++ models/user.js
@@ -1,24 +1,7 @@
 import mongoose from "mongoose";
 
-const validateEmail = function (email) {
-  const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  return re.test(email);
-};
-
 const userSchema = mongoose.Schema({
   username: String,
-  email: {
-    type: String,
-    trim: true,
-    lowercase: true,
-    unique: true,
-    required: "Email address is required",
-    validate: [validateEmail, "Please fill a valid email address"],
-    match: [
-      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-      "Please fill a valid email address",
-    ],
-  },
   password: String,
 });
 
