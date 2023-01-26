097d151d21c86b340083bc448bee1ed6a5276ccc
Patricio Albornoz <wagnerpatricio@outlook.com.ar>
2023-01-26 20:28:51 -0300

    twilio whatsapp working

--- helpers/twilioConfig.js
+++ helpers/twilioConfig.js
@@ -1,17 +1,17 @@
 import twilio from "twilio";
 import logger from "./logger.js";
 
-const accountSid = process.env.TWILIOACCOUNTSID;
-const authToken = process.env.TWILIOAUTHTOKEN;
+const accountSid = process.env.TWILIO_ACCOUNTS_ID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
 
 const client = twilio(accountSid, authToken);
 
 const sendSMS = async () => {
   try {
     const message = await client.messages.create({
-      body: "Hola soy un SMS desde node.js",
+      body: "Hola esto es un mensaje SMS desde Node.js",
       from: "+16083705783",
-      to: "+543435142341",
+      to: process.env.DEV_NUMBER,
     });
     console.log(message);
   } catch (error) {
@@ -19,4 +19,18 @@ const sendSMS = async () => {
   }
 };
 
-export const twilioService = {sendSMS}
+const sendWhatsapp = async () => {
+  try {
+    const message = await client.messages.create({
+      body: "Hola esto es un mensaje por Whatsapp desde Node.js!",
+      mediaUrl: [ 'https://raw.githubusercontent.com/pato-1441/bornozFly/main/public/image-6.png' ],
+      from: "whatsapp:+14155238886",
+      to: `whatsapp:${process.env.DEV_WHATSAPP_NUMBER}`,
+    });
+    console.log(message);
+  } catch (error) {
+    logger.error(error);
+  }
+};
+
+export const twilioService = { sendSMS, sendWhatsapp };
