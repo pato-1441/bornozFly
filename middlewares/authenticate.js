eca80cedb2460552f2e8d5a9af0a483e3fb6d5bf
Patricio Albornoz <wagnerpatricio@outlook.com.ar>
2023-01-26 09:32:31 -0300

    quitar clg

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -1,14 +1,4 @@
 const Authenticated = (req, res, next) => {
-  console.log(req.user);
-  /* const user = {
-    username: req.user.username,
-    password: req.user.password,
-    firstname: req.user.firstname,
-    lastname: req.user.lastname,
-    address: req.user.address,
-    birthday: req.user.birthday,
-    phone: req.user.phone,
-  }; */
   if (req.isAuthenticated())
     return res.render("home", {
       username: req.user.username,
